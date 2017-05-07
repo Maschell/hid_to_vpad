62ac147339e9f4d8ed19d9a7975e68de0c6f1eca
Maschell <Maschell@gmx.de>
2017-05-07 14:36:52 +0200

    Fix encoding of spanish.lang and updated the upadatelang script to not wrap lines

--- updatelang.sh
+++ updatelang.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 #
-find . -iname "*.cpp" | xargs xgettext -o languages/english.lang -j
+find . -iname "*.cpp" | xargs xgettext --no-wrap -o languages/english.lang -j 
 for fn in `find languages/*.lang`; do
     if [ "$fn" != "languages/english.lang" ]; then
-        msgmerge --output-file=$fn $fn languages/english.lang
+        msgmerge --output-file=$fn $fn languages/english.lang  --no-wrap
     fi
     
 done
