69a99a92e1255d24a5329ff677dafb74029b2c7e
Maschell <Maschell@gmx.de>
2017-05-08 17:23:14 +0200

    Added Brazilian Portuguese translations (thanks AlanLC), updated updatelang.sh script
    - changing order in lang files
    - fixed comment on english.lang

--- updatelang.sh
+++ updatelang.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
-find . -iname "*.cpp" | xargs xgettext --no-wrap -o languages/english.lang -j 
+find . -iname "*.cpp" | xargs xgettext --no-wrap -o languages/english.lang -j --no-location --omit-header
+find . -iname "*.cpp" | xargs xgettext --no-wrap -o languages/english.lang -j --omit-header --sort-by-file
 for fn in `find languages/*.lang`; do
     if [ "$fn" != "languages/english.lang" ]; then
         msgmerge --output-file=$fn $fn languages/english.lang  --no-wrap
