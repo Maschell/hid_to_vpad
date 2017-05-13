16d8f25a1fce4acc1dd5137f6fa6f54dd69991d8
Maschell <Maschell@gmx.de>
2017-05-13 15:29:03 +0200

    Fixing another stupid typo

--- ftpupload.sh
+++ ftpupload.sh
@@ -4,8 +4,10 @@
 cd $TRAVIS_BUILD_DIR/wiiu/apps/hidtovpad
 
 for entry in "$TRAVIS_BUILD_DIR/wiiu/apps/hidtovpad"/*; do
-    echo "Uploading ${entry##*/}"
-    curl --ftp-create-dirs -T "${entry##*/}" -u $FTP_USER:$FTP_PASSWORD "$FTP_SERVER/${entry##*/}"  > /dev/null 2>&1
+    if [ "$entry" != "languages" ] then
+        echo "Uploading ${entry##*/}"
+        curl --ftp-create-dirs -T "${entry##*/}" -u $FTP_USER:$FTP_PASSWORD "$FTP_SERVER/${entry##*/}"  > /dev/null 2>&1
+    fi
 done
 
 
@@ -29,7 +31,7 @@ done
 
 
 # Language files
-for f in `curl -u $FTP_USER:$FTP_PASSWORD $FTP_SERVER/sd/wiiu/controller/  --silent --use-ascii --list-only`; do
+for f in `curl -u $FTP_USER:$FTP_PASSWORD $FTP_SERVER/sd/wiiu/apps/hidtovpad/languages/  --silent --use-ascii --list-only`; do
     if [ "$f" != "." ] && [ "$f" != ".." ]; then
         echo "Deleting sd/wiiu/apps/hidtovpad/languages/${f##*/}"
         curl -v -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_SERVER/ -Q "DELE sd/wiiu/apps/hidtovpad/languages/$f" --silent > /dev/null 2>&1
