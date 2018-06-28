0df6c595f081000d03c0781f06f66f23e191d399
Maschell <Maschell@gmx.de>
2018-06-28 23:34:05 +0200

    Fix plugin to be compatible with the new loader

--- makefile.mk
+++ makefile.mk
@@ -1,12 +1,6 @@
 # Compiling the projects with libutils logging code?
 DO_LOGGING          := 1
 
-# Non WUT plugins need to wrap the malloc functions.
-WRAP_MALLOC         := 0
-
-# Sets the "-D__WUT__" compiling flag
-USE_WUT             := 1
-
 # Links against the wut implementation of newlib, this is useful for using any function
 # from the C standard library
 WUT_ENABLE_NEWLIB   := 0
@@ -36,16 +30,14 @@ INCLUDES            := src
 #---------------------------------------------------------------------------------
 # options for code generation and linking
 #---------------------------------------------------------------------------------
+# Extra C AND C++ compiler flags
+COMMON_CFLAGS       := 
 # Extra C compiler flags
-CFLAGS              := 
+CFLAGS              :=
 # Extra C++ compiler flags
-CXXFLAGS            := 
+CXXFLAGS            :=
 # Extra linking flags for all linking steps
-LD_FLAGS            := 
-# extra linking flags for linking the temporarily elf file (using ld)
-LD_FLAGS_ELF        := 
-# extra linking flags for linking the final mod file (using gcc/g++)
-LD_FLAGS_MOD        := 
+LDFLAGS             := 
 
 #---------------------------------------------------------------------------------
 # list of directories containing libraries, this must be the top level containing
@@ -56,7 +48,7 @@ LIBDIRS             := $(WUPSDIR) $(WUT_ROOT)
 #---------------------------------------------------------------------------------
 # any extra libraries we wish to link with the project
 #---------------------------------------------------------------------------------
-LIBS                := -lwups -lutilswut -lcontrollerpatcherwut -lcoreinit -lnsysnet -lnsyshid -lvpadbase
+LIBS                := -lwups -lcontrollerpatcherwut -lutilswut  -lcoreinit -lnsysnet -lnsyshid -lvpadbase
 
 #---------------------------------------------------------------------------------
 # Will be added to the final lib paths
@@ -64,7 +56,7 @@ LIBS                := -lwups -lutilswut -lcontrollerpatcherwut -lcoreinit -lnsy
 # -L$C:/library1/lib
 #---------------------------------------------------------------------------------
 EXTERNAL_LIBPATHS   := 
-
+                        
 #---------------------------------------------------------------------------------
 # Will be added to the final include paths
 # -IC:/library1/include
